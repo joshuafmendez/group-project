da0cff8b01724a4bc39d4fb39d4d07098397fa5c
Kobe <tahiriadam@gmail.com>
2021-07-20 19:49:56 -0400

    add new rout

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,40 +1,45 @@
-import { ReactRouter as Router, Switch, Route } from 'react-router-dom';
+import { ReactRouter as Router, Switch, Route } from "react-router-dom";
 
 //Components
-import NavBar from './components/NavBar.js';
+import NavBar from "./components/NavBar.js";
 
 //Pages
-import Four0Four from './pages/Four0Four.js';
-import Home from './pages/Home.js';
-import Index from './pages/Index.js';
-import Show from './pages/Show.js';
+import Home from "./pages/Home.js";
+import Index from "./pages/Index.js";
+import Show from "./pages/Show.js";
+import New from "./pages/New.js";
+import Edit from "./pages/Edit.js";
+import Four0Four from "./pages/Four0Four.js";
 
 import { apiURL } from "./util/apiURL.js";
 const API = apiURL();
 
 function App() {
-  
-
   return (
     <div>
-      <NavBar />
-      <Switch>
-        <Route exact to="/">
-          <Home />
-        </Route>
-        <Route exact to="/products">
-          <Index />
-        </Route>
-        <Route exact to="/:id">
-          <Show />
-        </Route>
-        <Route to="/:id/edit">
-          <Edit />
-        </Route>
-        <Route to="/*">
-          <Four0Four />
-        </Route>
-      </Switch>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact to="/">
+            <Home />
+          </Route>
+          <Route exact to="/products">
+            <Index />
+          </Route>
+          <Route exact to="/:id">
+            <Show />
+          </Route>
+          <Route to="/products/new">
+            <New />
+          </Route>
+          <Route to="/:id/edit">
+            <Edit />
+          </Route>
+          <Route to="/*">
+            <Four0Four />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
