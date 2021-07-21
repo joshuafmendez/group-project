a3e3d81b134b0ce37d955f0501a8402973260800
joshuamendez <joshuafmendez@gmail.com>
2021-07-20 20:27:37 -0400

    wireframe of products controller

--- back-end/controllers/productsController.js
+++ back-end/controllers/productsController.js
@@ -1,7 +1,35 @@
 const products = require("express").Router();
 
-products.get("/", (req,res)=>{
-    res.json("Help Me")
-})
+// const {} = require("../queries/products");
+
+products.get("/", (req, res) => {
+  try {
+    res.json({status: "success", payload: "Get All"});
+  } catch (err) {res.json({status: "success", payload: err})}
+});
+
+products.get("/:id", (req, res) => {
+  try {
+    res.json({status: "success", payload: "Get by ID"});
+  } catch (err) {res.json({status: "success", payload: err})}
+});
+
+products.post("/", (req, res) => {
+  try {
+    res.json({status: "success", payload: "Post"});
+  } catch (err) {res.json({status: "success", payload: err})}
+});
+
+products.delete("/:id", (req, res) => {
+  try {
+    res.json({status: "success", payload: "delete"});
+  } catch (err) {res.json({status: "success", payload: err})}
+});
+
+products.put("/:id", (req, res) => {
+  try {
+    res.json({status: "success", payload: "Edit"});
+  } catch (err) {res.json({status: "success", payload: err})}
+});
 
 module.exports = products;
