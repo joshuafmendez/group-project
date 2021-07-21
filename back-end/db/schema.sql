c5f6594dd594ea2543c2a726ab9535670681be22
joshuamendez <joshuafmendez@gmail.com>
2021-07-20 21:52:30 -0400

    code clean up

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,10 +1,3 @@
-DROP TABLE IF EXISTS test;
-
-CREATE TABLE test (
-    id SERIAL PRIMARY KEY, 
-    name TEXT
-);
-
 DROP TABLE if EXISTS emotions;
 
 CREATE TABLE emotions (
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,16 +1,3 @@
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
-
-
--- name TEXT NOT NULL,
---     price NUMERIC NOT NULL,
---     description TEXT NOT NULL
 INSERT INTO emotions (name, price, description) VALUES 
 ('Self-Confidence',10,'Are you tired of doubting yourself?'),
 ('Peace of Mind',1,'Stressed beyond your wildest dreams?'),
