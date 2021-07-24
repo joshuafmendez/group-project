43b2e60f2a6c473c297d598fe39c62958c12783a
Kobe <tahiriadam@gmail.com>
2021-07-24 16:23:37 -0400

    add sorting functionality

--- front-end/src/components/ProductList.js
+++ front-end/src/components/ProductList.js
@@ -1,35 +1,51 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { apiURL } from '../util/apiURL';
-import ProductListItem from './ProductListItem'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { apiURL } from "../util/apiURL";
+import ProductListItem from "./ProductListItem";
 
 const API = apiURL();
 
 const ProductList = ({ setCart }) => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        const getProducts = async () => {
-            try {
-                const res = await axios.get(`${API}/products`);
-                setProducts(res.data.payload);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        getProducts();
-    }, []);
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(`${API}/products`);
+        setProducts(res.data.payload);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
+  }, []);
 
+  const sortByAsc = () => { 
+    const sortedProducts = [...products].sort((a, b) => a.price - b.price)
+    setProducts(sortedProducts);
+  };
 
-    return (
-        <div>
-            <section>
-                {products.map(product => {
-                    return <ProductListItem key={product.id} product={product} setCart={setCart}/>
-                })}
-            </section>
-        </div>
-    )
-}
+  const sortByDesc = () => {
+    setProducts([...products].sort((a, b) => b.price - a.price));
+  };
 
-export default ProductList
+  return (
+    <div>
+      <button onClick={sortByAsc}>Ascending</button>
+      <button onClick={sortByDesc}>Descending</button>
+      <section>
+        {products.map((product) => {
+          return (
+            <ProductListItem
+              key={product.id}
+              product={product}
+              setCart={setCart}
+            />
+          );
+        })}
+      </section>
+    </div>
+  );
+};
+
+export default ProductList;
