1df492e037141e6f5223faaa24ede7699da79257
joshuamendez <joshuafmendez@gmail.com>
2021-07-24 17:39:13 -0400

    heroku bug fix

--- back-end/middleware/validation.js
+++ back-end/middleware/validation.js
@@ -4,16 +4,15 @@ const validation = (req, res, next) => {
   const emailValidation = (userEmail) => {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
   };
-console.log(req.route.path)
+  console.log(req.route.path);
   if (req.route.path === "/register") {
-
     //convert each element to a boolean
     if (![name, email, password].every(Boolean)) {
       return res.status(401).json("Missing Credentials");
     } else if (!emailValidation(email)) {
       return res.status(401).json("Invalid Email");
     }
-  } else if (req.originalUrl === "/login") {
+  } else if (req.route.path === "/login") {
     if (![email, password].every(Boolean)) {
       return res.status(401).json("Missing Credentials");
     } else if (!emailValidation(email)) {
